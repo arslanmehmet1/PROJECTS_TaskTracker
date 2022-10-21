6b9de005e16dfcb31bbfbb4cccd68de24a41717f
Mehmet Arslan <marslan.developer@gmail.com>
2022-10-21 12:25:55 +0300

    first

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Show Text Bar");
+  const [task, setTask] = useState();
+  const url = "https://6351821b3e9fa1244e60878b.mockapi.io/api/task";
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    setText(isOpen ? "Hide Text Bar" : "Show Text Bar");
+  };
+
+  //? READ
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const { data } = await axios(url);
+    setTask(data);
+    console.log(data);
+  };
+
   return (
     <div>
-      Home
-      <AddTask />
-      <TaskList />
+      <Button
+        variant="dark"
+        size="lg"
+        onClick={() => {
+          toggle();
+        }}
+      >
+        {text}
+      </Button>
+
+      {isOpen && <AddTask />}
+
+      <TaskList task={task} />
     </div>
   );
 };
